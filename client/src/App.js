88bba17fcba071482758362f74876ef43cebdf12
namdp109 <namdang109@protonmail.com>
2022-10-16 22:10:42 +0700

    init src

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Comments from './components/Comment';
+import Task from './components/Task';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/task" element={<Task />} />
+        <Route path="/comments/:category/:id" element={<Comments />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
